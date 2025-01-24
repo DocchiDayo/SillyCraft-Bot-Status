5c138f60b305fa0b524e9356c3a293f2e9d0d939
DocchiDayo <muhammad.dzakwan27@gmail.com>
2025-01-24 22:34:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
+const statusMessages = ["Watching Server 👀", "SillyCraft Bot by DOCCHI 🤖", "Helping Server 🐢", "No Freaky 👅"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
